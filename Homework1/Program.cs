3dc616572de716a3c0e4a1edc0d4257efb888d11
MeanNapat012 <napat.kemachaiwead@mail.kmutt.ac.th>
2022-11-20 22:48:22 +0700

    .

---
+++ Homework1/Program.cs
@@ -0,0 +1,85 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Queue<char> Flower = new Queue<char>();
+        char dokmai = 'a';
+        int Check1 = 0;
+        char dokmaiRose = 'b';
+        int i = 0;
+        do
+        {
+            
+            dokmai = char.Parse(Console.ReadLine());
+            if(dokmai == 'J')
+            {
+                if(dokmai != dokmaiRose)
+                {
+                    Flower.Push('J');
+                    Check1++;
+                }
+                else{
+                    Console.WriteLine("Invalid pattern");
+                    dokmaiRose = 'J';
+                }
+                    
+            }
+            else if(dokmai == 'G')
+            {
+                if(i < 3){
+                    if(dokmai != dokmaiRose)
+                    {
+                        Flower.Push('G');
+                        Check1++;
+                        i++;
+                    
+                    }
+                }
+                else if(i > 3)
+                {
+                    Console.WriteLine("Invalid pattern");
+                    dokmaiRose = 'G';
+                }
+                else{
+                    Console.WriteLine("Invalid pattern");
+                    dokmaiRose = 'G';
+                }
+                
+                
+            }
+            else if(dokmai == 'O')
+            {
+                if(dokmai != dokmaiRose)
+                {
+                    Flower.Push('O');
+                    Check1++;
+                }
+                else{
+                    Console.WriteLine("Invalid pattern");
+                    dokmaiRose = 'O';
+                }
+                
+            }
+            else if(dokmai == 'R')
+            {
+                if(Check1 == 0)
+                {
+                    Console.WriteLine("Invalid pattern");
+                }
+                else{
+                    Flower.Push('R');
+                    Check1++;
+                    dokmaiRose = Flower.Get(Flower.GetLength()- 2);
+                }
+                
+            }
+
+        }while(dokmai == 'J' || dokmai == 'G' || dokmai == 'O' || dokmai == 'R'||dokmai == 'a');
+
+        for(int a = 0; a < Flower.GetLength(); a++)
+        {
+            Console.Write("{0}", Flower.Get(a));
+        }
+
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args){
+        Queue<int> NumSize = new Queue<int>();
+        Queue<int> NumSize1 = new Queue<int>();
+        Queue<int> NumSize2 = new Queue<int>();
+        Char Size = 'b';
+        do
+        {
+            Size = char.Parse(Console.ReadLine());
+            if(Size == 'S')
+            {
+                NumSize.Push(3);
+                
+            }
+            else if(Size == 'M')
+            {
+                NumSize.Push(2);
+                for(int i = 0; i < 3; i++)
+                {
+                    NumSize1.Push(3);
+                }
+            }
+            else if(Size == 'L')
+            {
+                NumSize.Push(1);
+                for(int i = 0; i < 2; i++)
+                {
+                    NumSize1.Push(2);
+                    for(int j = 0; j < 3; j++)
+                    {
+                        NumSize2.Push(3);
+                    }
+                }
+            }
+
+        }while(Size == 'S'||Size == 'M'||Size == 'L'||Size == 'b');
+
+        for(int a = 0; a < NumSize.GetLength(); a++)
+        {
+            Console.Write("{0}", NumSize.Get(a));
+        }
+        for(int b = 0; b < NumSize1.GetLength(); b++)
+        {
+            Console.Write("{0}", NumSize1.Get(b));
+        }
+        for(int c = 0; c < NumSize2.GetLength(); c++)
+        {
+            Console.Write("{0}", NumSize2.Get(c));
+        }
+    }
+}
